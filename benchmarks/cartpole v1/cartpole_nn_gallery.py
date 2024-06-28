37174057d9493a78890a096952ad4870e456e2f3
amavrits <amavrits@gmail.com>
2024-06-28 15:30:18 +0200

    Started Vanilla PG implementation

--- benchmarks/cartpole v1/cartpole_nn_gallery.py
+++ benchmarks/cartpole v1/cartpole_nn_gallery.py
@@ -72,19 +72,30 @@ class PPO_NN_model(nn.Module):
     action_dim: Sequence[int]
     config: dict
 
-    # @nn.compact
-    # def __call__(self, x):
-    #     n_atoms = 51
-    #
-    #     q = nn.Dense(128, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(x)
-    #     q = nn.relu(q)
-    #     q = nn.Dense(64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(q)
-    #     q = nn.relu(q)
-    #     q = nn.Dense(32, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(q)
-    #     q = nn.relu(q)
-    #     q = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0))(q)
-    #
-    #     return q.reshape(-1, self.action_dim)
+    @nn.compact
+    def __call__(self, x):
+
+        activation = nn.tanh
+
+        actor_mean = nn.Dense(128, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(x)
+        actor_mean = activation(actor_mean)
+        actor_mean = nn.Dense(64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(actor_mean)
+        actor_mean = activation(actor_mean)
+        actor_mean = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0))(actor_mean)
+        pi = distrax.Categorical(logits=actor_mean)
+
+        critic = nn.Dense(128, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(x)
+        critic = activation(critic)
+        critic = nn.Dense(64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0))(critic)
+        critic = activation(critic)
+        critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(critic)
+
+        return pi, jnp.squeeze(critic, axis=-1)
+
+
+class VanillaPG_NN_model(nn.Module):
+    action_dim: Sequence[int]
+    config: dict
 
     @nn.compact
     def __call__(self, x):
---
+++ benchmarks/cartpole v1/cartpole_v1_vanillapg.py
@@ -0,0 +1,96 @@
+import os
+import time
+import jax
+import optax
+import gymnax
+import numpy as np
+from jaxagents import vanillapg as vpg
+from cartpole_nn_gallery import *
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    env, env_params = gymnax.make("CartPole-v1")
+
+    """Set up transition template, given the state representation in the cartpole environment."""
+    transition_temp = vpg.Transition(
+        state=jnp.zeros((1, 4), dtype=jnp.float32),
+        action=jnp.zeros(1, dtype=jnp.int32),
+        log_prob=jnp.zeros(1, dtype=jnp.float32),
+        value=jnp.zeros(1, dtype=jnp.float32),
+        reward=jnp.zeros(1, dtype=jnp.float32),
+        next_state=jnp.zeros((1, 4), dtype=jnp.float32),
+        terminated=jnp.zeros(1, dtype=jnp.bool_),
+        info={
+            "discount": jnp.array((), dtype=jnp.float32),
+            "returned_episode": jnp.array((), dtype=jnp.bool_),
+            "returned_episode_lengths": jnp.array((), dtype=jnp.int32),
+            "returned_episode_returns": jnp.array((), dtype=jnp.float32),
+        }
+    )
+
+
+    """Define configuration for agent training"""
+    config = vpg.AgentConfig(
+        ac_network=VanillaPG_NN_model,
+        transition_template=transition_temp,
+        n_steps=100_000,
+        update_epochs=1,
+        batch_size=64,
+        store_agent=False,
+        act_randomly=lambda random_key, state, n_actions: jax.random.choice(random_key, jnp.arange(n_actions)),
+        get_performance=lambda i_step, step_runner: 0,
+        optimizer=optax.rmsprop,
+        loss_fn=optax.l2_loss
+    )
+
+
+    """Set up agent"""
+    agent = vpg.VanillaPGAgent(env, env_params, config)
+    print(agent.__str__())
+
+
+    """Define optimizer parameters and training hyperparameters"""
+    optimizer_params = vpg.OptimizerParams(learning_rate=5e-3, eps=1e-3, grad_clip=1)
+    hyperparams = vpg.HyperParameters(gamma=0.99,
+                                      gae_lambda=0.95,
+                                      clip_eps=0.05,
+                                      vf_coeff=0.5,
+                                      ent_coeff=0.01,
+                                      optimizer_params=optimizer_params)
+
+
+    """Draw random key"""
+    rng = jax.random.PRNGKey(42)
+    rng_train, rng_eval = jax.random.split(rng)
+
+    """Train agent"""
+    t0 = time.time()
+    runner, training_metrics = jax.block_until_ready(agent.train(rng_train, hyperparams))
+    print(f"time: {time.time() - t0:.2f} s")
+
+    """ Post-process results"""
+    agent.collect_training(runner)
+    training_rewards = agent.summarize(training_metrics["done"].flatten(), training_metrics["reward"].flatten())
+
+    """Evaluate agent performance"""
+    eval_metrics = agent.eval(rng_eval, n_evals=500_000)
+    eval_rewards = agent.summarize(eval_metrics["done"].flatten(), eval_metrics["reward"].flatten())
+
+    """ Plot results"""
+    running_window = 100
+    running_training_rewards = (np.cumsum(training_rewards.episode_metric)[running_window:] -
+                                np.cumsum(training_rewards.episode_metric)[:-running_window]) / running_window
+    running_eval_rewards = (np.cumsum(eval_rewards.episode_metric)[running_window:] -
+                                np.cumsum(eval_rewards.episode_metric)[:-running_window]) / running_window
+
+    fig = plt.figure()
+    plt.plot(training_rewards.episode_metric, c='b', alpha=0.4)
+    plt.plot(np.arange(running_window, training_rewards.episode_metric.size), running_training_rewards, c='b')
+    plt.xlabel("Episode", fontsize=14)
+    plt.ylabel("Reward [-]", fontsize=14)
+    plt.close()
+    fig.savefig(os.path.join(os.getcwd(), r'figs\vpg training.png'))
