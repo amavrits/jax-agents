ccf062a367710d104133f7ab3ca587c99f22d2d4
amavrits <amavrits@gmail.com>
2024-09-22 18:09:24 +0200

    Used vmap in ppo for: * _actor_loss * _critic_loss * _add_next_values

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "dev": []
     },
     python_requires=">=3.9.0",
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
     description='Implementation of Reinforcement Learning agents in JAX',
 )
