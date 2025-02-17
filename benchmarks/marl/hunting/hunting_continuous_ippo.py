Fixed issue with stop gradients in IPPO (both actor and critic). Found good training hyperparameters for hunting game.

benchmarks/marl/hunting/hunting_continuous_ippo.py
@@ -1,6 +1,4 @@
-import os, sys
-# os.environ["XLA_FLAGS"] = ('--xla_gpu_autotune_level=0')
-
+import sys
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -14,6 +12,7 @@ from hunting_gallery import PGActorContinuous, PGCritic
 from jax_tqdm import scan_tqdm
 from functools import partial
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class HuntingIPPO(IPPO):
@@ -24,11 +23,12 @@ class HuntingIPPO(IPPO):
         pis = distrax.Normal(loc=mus, scale=jnp.exp(-0.5))
         return pis.entropy()
 
-    @partial(jax.jit, static_argnums=(0,))
-    def _log_prob(self, actor_training: TrainState, state: STATE_TYPE, actions: Int[Array, "n_actors"])\
+    @partial(jax.jit, static_argnums=(0, 4,))
+    def _log_prob(self, actor_training: TrainState, params: dict, state: STATE_TYPE, actions: Int[Array, "n_actors"])\
             -> Float[Array, "n_actors"]:
-        mus = actor_training.apply_fn(actor_training.params, state).squeeze()
+        mus = actor_training.apply_fn(params, state).squeeze()
         log_probs = distrax.Normal(loc=mus, scale=jnp.exp(-0.5)).log_prob(actions)
+        # log_probs = distrax.ClippedNormal(loc=mus, scale=jnp.exp(-0.5), minimum=-jnp.pi, maximum=jnp.pi).log_prob(actions)
         return log_probs
 
     @partial(jax.jit, static_argnums=(0,))
@@ -39,13 +39,22 @@ class HuntingIPPO(IPPO):
     @partial(jax.jit, static_argnums=(0,))
     def _sample_actions(self, rng: PRNGKeyArray, actor_training: TrainState, state: STATE_TYPE)\
         -> Tuple[PRNGKeyArray, List[Int[Array, "1"]]]:
-        mus = actor_training.apply_fn(actor_training.params, state).squeeze()
+        mus = actor_training.apply_fn(jax.lax.stop_gradient(actor_training.params), state).squeeze()
         # Use fixed std, OpenAI: https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/core.py#L84
         actions = distrax.Normal(loc=mus, scale=jnp.exp(-0.5)).sample(seed=rng)
+        # actions = distrax.ClippedNormal(loc=mus, scale=jnp.exp(-0.5), minimum=-jnp.pi, maximum=jnp.pi).sample(seed=rng)
         return actions
 
 
 def best_predator_win(x):
+    # terminated = x["terminated"]
+    # terminated_pred_rewards = jnp.where(terminated, jnp.take(x["final_rewards"], 1, axis=-1), 0)
+    # terminated_pred_wins = jnp.where(jnp.equal(terminated_pred_rewards, 10), 1, 0)
+    # pred_win_ratio = jnp.where(
+    #     jnp.equal(terminated.sum(axis=-1), 0),
+    #     terminated_pred_wins.sum(axis=-1) / terminated.sum(axis=-1),
+    #     0
+    # )
     pred_win_ratio = jnp.mean(jnp.equal(jnp.take(x["final_rewards"], 1, axis=-1), 10), axis=-1)
     return jnp.argmax(pred_win_ratio).item()
 
@@ -53,9 +62,11 @@ def best_predator_win(x):
 def plot_training(training_metrics, eval_frequency, env_params, path):
     rewards_prey = training_metrics["final_rewards"][..., 0]
     rewards_pred = training_metrics["final_rewards"][..., 1]
-    p_prey = np.mean(training_metrics["final_rewards"][..., 0] != -env_params.caught_reward, axis=1) * 100
-    p_pred = np.mean(training_metrics["final_rewards"][..., 1] == env_params.caught_reward, axis=1) * 100
-    steps = jnp.arange(1, rewards_pred.shape[0]+1) * eval_frequency
+    # p_prey = np.mean(rewards_prey != -env_params.caught_reward, axis=1) * 100
+    # p_pred = np.mean(rewards_pred == env_params.caught_reward, axis=1) * 100
+    p_prey = np.mean(rewards_prey > env_params.predator_radius, axis=1) * 100
+    p_pred = np.mean(-rewards_pred <= env_params.predator_radius, axis=1) * 100
+    steps = jnp.arange(rewards_pred.shape[0]) * eval_frequency
     fig, axs = plt.subplots(2, 2, sharex=True, figsize=(12, 8))
     axs[0, 0].plot(steps, rewards_prey.mean(axis=1), c="b")
     axs[0, 0].fill_between(steps, rewards_prey.min(axis=1), rewards_prey.max(axis=1), color="b", alpha=0.3)
@@ -69,15 +80,35 @@ def plot_training(training_metrics, eval_frequency, env_params, path):
     axs[1, 1].set_xlabel("Training steps", fontsize=12)
     axs[0, 1].set_ylabel("Prey\nStalemate ratio [%]", fontsize=12)
     axs[1, 1].set_ylabel("Predator\nWin ratio [%]", fontsize=12)
+    axs[0, 1].set_ylim(0, 100)
+    axs[1, 1].set_ylim(0, 100)
     for ax in axs.flatten():
         ax.grid()
     plt.close()
     fig.savefig(path)
 
 
+def export_csv(render_metrics, csv_path):
+    df = pd.DataFrame(
+        data = np.round(np.c_[
+            render_metrics["step"],
+            render_metrics["time"],
+            render_metrics["positions"].reshape(n_eval_steps, 4),
+            render_metrics["actions"],
+            render_metrics["values"],
+            render_metrics["next_positions"].reshape(n_eval_steps, 4),
+            render_metrics["reward"],
+            render_metrics["terminated"],
+        ], 2),
+    columns=["Step", "Time"]+["Positions_"+str(i+1) for i in range(4)]+["Action1", "Action2", "Value1", "Value2"]+
+            ["Next_positions_" + str(i + 1) for i in range(4)]+["Reward1", "Reward2", "Terminated"]
+    )
+    df.to_csv(csv_path, index=False)
+
+
 if __name__ == "__main__":
 
-    env_params = EnvParams(prey_velocity=2, predator_velocity=1)
+    env_params = EnvParams(prey_velocity=1., predator_velocity=1.)
     env = HuntingContinuous(allow_timeover=False)
 
     if sys.platform == "win32":
@@ -87,30 +118,31 @@ if __name__ == "__main__":
 
     config = IPPOConfig(
         n_steps=1_000,
-        batch_size=256,
+        batch_size=128,
         minibatch_size=16,
-        rollout_length=100,
-        actor_epochs=50,
-        critic_epochs=50,
+        rollout_length=300,
+        actor_epochs=30,
+        critic_epochs=30,
         actor_network=PGActorContinuous,
         critic_network=PGCritic,
         optimizer=optax.adam,
-        eval_frequency=20,
+        eval_frequency=100,
         eval_rng=jax.random.PRNGKey(18),
         n_evals=100,
-        # checkpoint_dir=checkpoint_dir,
-        # restore_agent=True
+        checkpoint_dir=checkpoint_dir,
+        restore_agent=False,
+        # restore_agent=True,
     )
 
     hyperparams = HyperParameters(
         gamma=0.99,
-        eps_clip=0.05,
+        eps_clip=0.1,
         kl_threshold=1e-5,
         gae_lambda=0.97,
-        ent_coeff=0.001,
+        ent_coeff=0.01,
         vf_coeff=1.0,
-        actor_optimizer_params=OptimizerParams(learning_rate=5e-3, eps=1e-3, grad_clip=1),
-        critic_optimizer_params=OptimizerParams(learning_rate=1e-3, eps=1e-3, grad_clip=1)
+        actor_optimizer_params=OptimizerParams(learning_rate=3e-4, eps=1e-5, grad_clip=1),
+        critic_optimizer_params=OptimizerParams(learning_rate=1e-4, eps=1e-5, grad_clip=1)
     )
 
     ippo = HuntingIPPO(env, env_params, config, eval_during_training=True)
@@ -122,12 +154,16 @@ if __name__ == "__main__":
     runner, training_metrics = jax.block_until_ready(ippo.train(rng_train, hyperparams))
     eval_metrics = jax.block_until_ready(ippo.eval(rng_eval, runner.actor_training, n_evals=16))
 
+    training_plot_path = r"figures/continuous/ippo_continuous_policy_training_{steps}steps.png".format(steps=config.n_steps)
+    plot_training(training_metrics, config.eval_frequency, env_params, training_plot_path)
+
     def f(runner, i):
-        rng, actor_training, state, state_env = runner
+        rng, actor_training, critic_training, state, state_env = runner
         actions = ippo.policy(actor_training, state)
+        values = critic_training.apply_fn(critic_training.params, state)
         rng, rng_step = jax.random.split(rng)
         next_state, next_env_state, reward, terminated, info = env.step(rng_step, state_env, actions, env_params)
-        runner = rng, actor_training, next_state, next_env_state
+        runner = rng, actor_training, critic_training, next_state, next_env_state
         metrics = {
             "step": i,
             "time": state_env.time,
@@ -136,19 +172,20 @@ if __name__ == "__main__":
             "next_positions": next_env_state.positions.reshape(-1, 2, 2),
             "reward": reward,
             "terminated": terminated,
+            "values": values,
         }
         return runner, metrics
 
-    n_eval_steps = 100
+    n_eval_steps = 500
     rng = jax.random.PRNGKey(43)
     state, state_env = env.reset(rng, env_params)
-    render_runner = rng, runner.actor_training, state, state_env
+    render_runner = rng, runner.actor_training, runner.critic_training, state, state_env
     render_runner, render_metrics = jax.lax.scan(scan_tqdm(n_eval_steps)(f), render_runner, jnp.arange(n_eval_steps))
     render_metrics = {key: np.asarray(val) for (key, val) in render_metrics.items()}
 
-    training_plot_path = r"figures/continuous/ippo_continuous_policy_training_{steps}steps.png".format(steps=config.n_steps)
-    plot_training(training_metrics, config.eval_frequency, env_params, training_plot_path)
+    csv_path = r"figures/continuous/details.csv"
+    export_csv(render_metrics, csv_path)
 
     gif_path = r"figures/continuous/ippo_continuous_policy_{steps}steps.gif".format(steps=config.n_steps)
-    env.animate(render_metrics["positions"], render_metrics["actions"], env_params, gif_path)
+    env.animate(render_metrics["positions"], render_metrics["actions"], render_metrics["values"], env_params, gif_path, export_pdf=True)
 

benchmarks/marl/hunting/hunting_env.py
@@ -10,6 +10,8 @@ from typing import Tuple
 from abc import abstractmethod
 from functools import partial
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 from PIL import Image
 import io
 
@@ -36,10 +38,12 @@ class EnvParams:
     predator_velocity: float = 1.
     predator_radius: float = .1
     max_time: float = 1.
-    dt: float = 1e-2
-    caught_reward: float = 10.
-    x_lims: Tuple[float, float] = (0., 1.)
-    y_lims: Tuple[float, float] = (0., 1.)
+    dt: float = .01
+    caught_reward: float = 1.
+    # x_lims: Tuple[float, float] = (0., 1.)
+    # y_lims: Tuple[float, float] = (0., 1.)
+    x_lims: Tuple[float, float] = (0., .5)
+    y_lims: Tuple[float, float] = (0., .5)
 
 
 class HuntingBase(environment.Environment):
@@ -50,6 +54,7 @@ class HuntingBase(environment.Environment):
 
     def get_obs(self, state: EnvState) -> STATE:
         return jnp.hstack((jnp.expand_dims(state.time, axis=-1), state.positions.reshape(1, -1)), dtype=jnp.float32)
+        # return state.positions.reshape(1, -1)
 
     def reset_env(self, key: chex.PRNGKey, env_params: Optional[EnvParams] = None) -> Tuple[STATE, EnvState]:
 
@@ -67,6 +72,10 @@ class HuntingBase(environment.Environment):
 
         return self.get_obs(state), state
 
+    @abstractmethod
+    def _adjust_bnds(self, positions: POSITIONS, env_params: EnvParams):
+        raise NotImplementedError
+
     def update_positions(self, positions:POSITIONS, directions: DIRECTIONS, velocities: VELOCITIES,
                          env_params: EnvParams) -> POSITIONS:
 
@@ -74,10 +83,7 @@ class HuntingBase(environment.Environment):
 
         next_positions = positions + displacement
 
-        next_positions = jnp.stack((
-            jnp.clip(jnp.take(next_positions, 0, axis=1), env_params.x_lims[0], env_params.x_lims[1]),
-            jnp.clip(jnp.take(next_positions, 1, axis=1), env_params.y_lims[0], env_params.y_lims[1])
-        ), axis=1)
+        next_positions = self._adjust_bnds(next_positions, env_params)
 
         return next_positions
 
@@ -88,11 +94,12 @@ class HuntingBase(environment.Environment):
                 timeover: Bool[Array, "1"], env_params: EnvParams) -> REWARDS:
 
         reward_prey = jnp.where(prey_caught, -env_params.caught_reward, distance)
-        reward_predator = -reward_prey
+        reward_predator = jnp.where(prey_caught, env_params.caught_reward, -distance)
         rewards = jnp.stack((reward_prey, reward_predator), axis=-1).squeeze()
+        # rewards = jnp.stack((distance, -distance), axis=-1).squeeze()
 
-        rewards_time_over = jnp.asarray([env_params.caught_reward, -env_params.caught_reward]).squeeze()
-        rewards = jnp.where(timeover, rewards_time_over, rewards)
+        # rewards_time_over = jnp.asarray([env_params.caught_reward, -env_params.caught_reward]).squeeze()
+        # rewards = jnp.where(timeover, rewards_time_over, rewards)
 
         return rewards
 
@@ -132,10 +139,11 @@ class HuntingBase(environment.Environment):
     def _directions(self, actions: ACTIONS) -> DIRECTIONS:
         raise NotImplementedError
 
-    def render(self, positions: POSITIONS, actions: ACTIONS, env_params: EnvParams) -> plt.Figure:
+    def render(self, positions: POSITIONS, actions: ACTIONS, values: REWARDS, env_params: EnvParams) -> plt.Figure:
 
         positions = positions.squeeze()
         actions = actions.squeeze()
+        values = values.squeeze()
         directions = self._directions(actions)
         length = 0.05
         dx = length * jnp.take(directions, 0, axis=-1)
@@ -149,6 +157,8 @@ class HuntingBase(environment.Environment):
         ax.scatter(positions[1, 0], positions[1, 1], c="r", s=80, label="Predator")
         ax.arrow(positions[0, 0], positions[0, 1], dx[0], dy[0], color="b", linewidth=1)
         ax.arrow(positions[1, 0], positions[1, 1], dx[1], dy[1], color="r", linewidth=1)
+        ax.annotate("${V}_{s}$"+"={value:.2f}".format(value=values[0]), (positions[0, 0], positions[0, 1]+0.01), color="b")
+        ax.annotate("${V}_{s}$"+"={value:.2f}".format(value=values[1]), (positions[1, 0], positions[1, 1]+0.01), color="r")
         pred_circle = plt.Circle((positions[1, 0], positions[1, 1]), env_params.predator_radius, color='r', alpha=0.3)
         ax.add_patch(pred_circle)
         ax.set_xlim(min(env_params.x_lims), max(env_params.x_lims))
@@ -164,9 +174,10 @@ class HuntingBase(environment.Environment):
 
         return fig
 
-    def animate(self, positions: POSITIONS, actions: ACTIONS, env_params: EnvParams, gif_path: str) -> None:
+    def animate(self, positions: POSITIONS, actions: ACTIONS, values: REWARDS, env_params: EnvParams, gif_path: str,
+                export_pdf: bool = False) -> None:
 
-        figs = list(map(lambda x, y: self.render(x, y, env_params), positions, actions))
+        figs = list(map(lambda x, y, z: self.render(x, y, z, env_params), positions, actions, values))
 
         image_frames = []
         for fig in figs:
@@ -185,6 +196,12 @@ class HuntingBase(environment.Environment):
             loop=0
         )
 
+        if export_pdf:
+            pdf_path = gif_path[:-3]+"pdf"
+            pp = PdfPages(pdf_path)
+            [pp.savefig(fig) for fig in figs]
+            pp.close()
+
     @property
     def name(self) -> str:
         return "Hunting"
@@ -199,6 +216,13 @@ class HuntingDiscrete(HuntingBase):
         directions = jnp.stack((jnp.cos(directions_rad), jnp.sin(directions_rad)), axis=-1)
         return directions
 
+    def _adjust_bnds(self, positions: POSITIONS, env_params: EnvParams):
+        positions = jnp.stack((
+            jnp.clip(jnp.take(positions, 0, axis=1), env_params.x_lims[0], env_params.x_lims[1]),
+            jnp.clip(jnp.take(positions, 1, axis=1), env_params.y_lims[0], env_params.y_lims[1])
+        ), axis=1)
+        return positions
+
 
 class HuntingContinuous(HuntingBase):
 
@@ -206,6 +230,13 @@ class HuntingContinuous(HuntingBase):
         actions_clip = jnp.clip(actions, -jnp.pi, +jnp.pi)
         directions = jnp.stack((jnp.cos(actions_clip), jnp.sin(actions_clip)), axis=-1)
         return directions
+
+    def _adjust_bnds(self, positions: POSITIONS, env_params: EnvParams):
+        positions = jnp.stack((
+            jnp.clip(jnp.take(positions, 0, axis=1), env_params.x_lims[0], env_params.x_lims[1]),
+            jnp.clip(jnp.take(positions, 1, axis=1), env_params.y_lims[0], env_params.y_lims[1])
+        ), axis=1)
+        return positions
 
 
 if __name__ == "__main__":

jaxagents/ippo.py
@@ -441,7 +441,7 @@ class IPPOBase(ABC):
                          critic_training: TrainState) -> Transition:
 
         last_state_value_vmap = jax.vmap(critic_training.apply_fn, in_axes=(None, 0))
-        last_state_value = last_state_value_vmap(jax.lax.stop_gradient(critic_training.params), last_state)
+        last_state_value = last_state_value_vmap(lax.stop_gradient(critic_training.params), last_state)
         last_state_value = jnp.expand_dims(last_state_value, axis=1)
 
         """Remove first entry so that the next state values per step are in sync with the state rewards."""
@@ -493,7 +493,7 @@ class IPPOBase(ABC):
 
         traj_runner = (rewards_t, discounts_t, next_state_values_t, gae_lambda)
         end_value = jnp.take(next_state_values_t, -1, axis=0)  # Start from end of trajectory and work in reverse.
-        _, returns = jax.lax.scan(self._trajectory_returns, end_value, traj_runner, reverse=True)
+        _, returns = lax.scan(self._trajectory_returns, end_value, traj_runner, reverse=True)
 
         returns = jax.tree_util.tree_map(lambda x: jnp.swapaxes(x, 0, 1), returns)
 
@@ -594,9 +594,9 @@ class IPPOBase(ABC):
         rng, rng_action = jax.random.split(rng)
         actions = self._sample_actions(rng_action, actor_training, state)
 
-        values = critic_training.apply_fn(critic_training.params, state)
+        values = critic_training.apply_fn(lax.stop_gradient(critic_training.params), state)
 
-        log_prob = self._log_prob(actor_training, state, actions)
+        log_prob = self._log_prob(actor_training, lax.stop_gradient(actor_training.params), state, actions)
 
         rng, next_state, next_env_state, reward, terminated, info = self._env_step(rng, env_state, actions)
 
@@ -1031,8 +1031,8 @@ class IPPOBase(ABC):
         raise NotImplemented
 
     @abstractmethod
-    def _log_prob(self, actor_training: TrainState, state: STATE_TYPE, actions: Int[Array, "n_actors"])\
-            -> Float[Array, "n_actors"]:
+    def _log_prob(self, actor_training: TrainState, params: Union[dict, FrozenDict], state: STATE_TYPE,
+                  actions: Int[Array, "n_actors"]) -> Float[Array, "n_actors"]:
         raise NotImplemented
 
     @abstractmethod
@@ -1281,10 +1281,8 @@ class IPPO(IPPOBase):
         """ Standardize GAE, greatly improves behaviour"""
         advantage = (advantage - advantage.mean(axis=0)) / (advantage.std(axis=0) + 1e-8)
 
-        # actor_policy_vmap = jax.vmap(jax.vmap(training.apply_fn, in_axes=(None, 0)), in_axes=(None, 0))
-        # actor_policy = actor_policy_vmap(training.params, state)
-        log_prob_vmap = jax.vmap(jax.vmap(self._log_prob, in_axes=(None, 0, 0)), in_axes=(None, 0, 0))
-        log_prob = log_prob_vmap(training, state, actions)
+        log_prob_vmap = jax.vmap(jax.vmap(self._log_prob, in_axes=(None, None, 0, 0)), in_axes=(None, None, 0, 0))
+        log_prob = log_prob_vmap(training, training.params, state, actions)
         log_policy_ratio = log_prob - log_prob_old
         policy_ratio = jnp.exp(log_policy_ratio)
         kl = jnp.sum(-log_policy_ratio)
@@ -1318,7 +1316,6 @@ class IPPO(IPPOBase):
         :return: The critic loss.
         """
 
-        # value = training.apply_fn(training.params, state)
         value_vmap = jax.vmap(jax.vmap(training.apply_fn, in_axes=(None, 0)), in_axes=(None, 0))
         value = value_vmap(training.params, state)
         residuals = value - targets

jaxagents/ppo.py
@@ -479,9 +479,8 @@ class PPOAgentBase(ABC):
     def _add_next_values(self, traj_batch: Transition, last_state: Float[Array, 'state_size'],
                          critic_training: TrainState) -> Transition:
 
-        # last_state_value = critic_training.apply_fn(jax.lax.stop_gradient(critic_training.params), last_state)
         last_state_value_vmap = jax.vmap(critic_training.apply_fn, in_axes=(None, 0))
-        last_state_value = last_state_value_vmap(jax.lax.stop_gradient(critic_training.params), last_state)
+        last_state_value = last_state_value_vmap(lax.stop_gradient(critic_training.params), last_state)
 
         """Remove first entry so that the next state values per step are in sync with the state rewards."""
         next_values_t = jnp.concatenate(
@@ -531,7 +530,7 @@ class PPOAgentBase(ABC):
 
         traj_runner = (rewards_t, discounts_t, next_state_values_t, gae_lambda)
         end_value = jnp.take(next_state_values_t, -1, axis=0)  # Start from end of trajectory and work in reverse.
-        _, returns = jax.lax.scan(self._trajectory_returns, end_value, traj_runner, reverse=True)
+        _, returns = lax.scan(self._trajectory_returns, end_value, traj_runner, reverse=True)
 
         returns = jax.tree_util.tree_map(lambda x: jnp.swapaxes(x, 0, 1), returns)
 